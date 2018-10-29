befad3e5bcb6f29ef7f28c83c742c28f4ebf4c37
yanske1 <yansongke1@gmail.com>
2018-10-28 23:10:47 -0700

    joining games

--- server/api/controller.py
+++ server/api/controller.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from models import Game
+from flask import Flask, request
+
+from models import Game, User
 from api import app, socketio
+import json
 
 global games
 games = {}
@@ -9,17 +11,30 @@ games = {}
 def index():
   return 'Hello, World!'
 
-@app.route('/game/<key>')
-def get_game(key):
-  output = ""
+@app.route('/game/<key>', methods=["post"])
+def join_game(key):
+  debug = ""
+  data = request.json
 
+  # Get or create game
   if key not in games:
     games[key] = Game(key)
-    output += "Invalid Key! Game created \n"
+    debug += "Invalid Key! Game created \n"
   
   game = games[key]
-  output += game.serialize() + '\n'
-  return output
+
+  # Create player
+  if "user" not in data:
+    debug += "invalid data!"
+    return debug
+
+  user = User(data["user"])
+  if not game.add_user(user):
+    debug += "failed to add"
+    return debug
+
+  debug += game.serialize() + '\n'
+  return debug
 
 @socketio.on('message')
 def handle_message(message):
